04b31bed9878f5ccd9bee9c61967605e7eb394e3
mwyrodek <wyrodek.maciej@gmail.com>
2018-05-07 19:45:16 +0200

    some how my commits from mac cant go throg re pushing them here

---
+++ ShipsWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ships.GameLoop;
+
+namespace ShipsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GameLoop game;
+
+        public ActionResult Index(string command)
+        {
+            if (TempData.ContainsKey("Game"))
+            {
+                //If so access it here
+                game = TempData["Game"] as GameLoop;
+            }
+            else
+            {
+                game = new GameLoop();
+            }
+            ViewBag.Message = $"{game.Act(command)}";
+            TempData["Game"] = game;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ShipsWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShipsWeb.MvcApplication" Language="C#" %>
